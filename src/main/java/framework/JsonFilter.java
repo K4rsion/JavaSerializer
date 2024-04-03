e5198b6908c00cea837e0d0c82928dca3651eb32
Igor Zhuravskiy <91545878+buljad@users.noreply.github.com>
2024-04-03 10:37:32 +0700

    Update JsonFilter with new javadoc

--- src/main/java/framework/JsonFilter.java
+++ src/main/java/framework/JsonFilter.java
@@ -5,30 +5,29 @@ import org.json.JSONException;
 import org.json.JSONObject;
 import org.json.JSONTokener;
 
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
 
 /**
- * JsonFilter represents class which can filter JSON objects without serializing them.
- * There are two ways of using filter() method - with Operator and String operator.
+ * This class has been written for filtering JSON files.
  */
 public class JsonFilter {
-
     /**
-     * Filter objects from JSON file without serializing them.
-     *
-     * @param inputFilePath Input file path.
-     * @param field         Field to compare with.
-     * @param operator      Operator to compare.
-     * @param fieldValue    Needed value of field.
-     * @return ArrayList of filtered objects.
+     * This is the main method that we can call to filter an input JSON file.
+     * @param inputFile - input JSON file
+     * @param field - field in JSON for comparing value of it and filtering
+     * @param operator - comparison operator(here we use its value from enum)
+     * @param fieldValue - value of the above-mentioned field
+     * @return - returns HashMap of filtered JSON data
      */
-    public static ArrayList<HashMap<String, Object>> filter(String inputFilePath, String field, Operator operator, String fieldValue) {
+    public static ArrayList<HashMap<String, Object>> filter(File inputFile, String field, Operator operator, String fieldValue) {
         ArrayList<JSONObject> filteredData = new ArrayList<>();
         try {
-            FileReader reader = new FileReader(inputFilePath);
+            FileReader reader = new FileReader(inputFile);
             JSONTokener token = new JSONTokener(reader);
             JSONArray jsonArray = new JSONArray(token);
             String[] fieldParts = field.split("\\.");
@@ -62,25 +61,28 @@ public class JsonFilter {
 
         ArrayList<HashMap<String, Object>> hashedFilteredData = new ArrayList<>();
         for (var e : filteredData) {
-            hashedFilteredData.add(JsonReader.toMap(e));
+            try {
+                hashedFilteredData.add(toMap(e));
+            } catch (JSONException exception) {
+                System.err.println("Error trying to convert JSON element");
+            }
         }
 
         return hashedFilteredData;
     }
 
     /**
-     * Filter objects from JSON file without serializing them.
-     *
-     * @param inputFilePath Input file path.
-     * @param field         Field to compare with.
-     * @param operator      Operator to compare.
-     * @param fieldValue    Needed value of field.
-     * @return ArrayList of filtered objects.
+     * Same method as previous, but for String value of operator
+     * @param inputFile - input JSON file
+     * @param field - field in JSON for comparing value of it and filtering
+     * @param operator - comparison operator(here we use its String value)
+     * @param fieldValue - value of the above-mentioned field
+     * @return - returns HashMap of filtered JSON data
      */
-    public static ArrayList<HashMap<String, Object>> filter(String inputFilePath, String field, String operator, String fieldValue) {
+    public static ArrayList<HashMap<String, Object>> filter(File inputFile, String field, String operator, String fieldValue) {
         ArrayList<JSONObject> filteredData = new ArrayList<>();
         try {
-            FileReader reader = new FileReader(inputFilePath);
+            FileReader reader = new FileReader(inputFile);
             JSONTokener token = new JSONTokener(reader);
             JSONArray jsonArray = new JSONArray(token);
             String[] fieldParts = field.split("\\.");
@@ -113,22 +115,34 @@ public class JsonFilter {
         }
         ArrayList<HashMap<String, Object>> hashedFilteredData = new ArrayList<>();
         for (var e : filteredData) {
-            hashedFilteredData.add(JsonReader.toMap(e));
+            try {
+                hashedFilteredData.add(toMap(e));
+            } catch (JSONException exception) {
+                System.err.println("Error trying to convert JSON element");
+            }
         }
         return hashedFilteredData;
     }
 
     /**
-     * Check if objects from JSON file have the "field" parameter.
-     *
-     * @param inputFilePath File path to read objects from.
-     * @param field         Field to check for entry.
-     * @return True - if field exists, else - False.
+     * This method checks for availability of field in the objects of JSON file
+     * @param inputFile - input JSON file
+     * @param field - field to check
+     * @return - true if there is a field in the file, else false
      */
-    public static Boolean inside(String inputFilePath, String field) {
-        return !filter(inputFilePath, field, "in", null).isEmpty();
+    public static Boolean inside(File inputFile, String field) {
+        return !filter(inputFile, field, "in", null).isEmpty();
     }
 
+    /**
+     * This method checks for correctness of JSON object for adding to filtered data
+     * @param jsonObject - JSON object to check
+     * @param field - field in JSON for comparing value of it and filtering
+     * @param operator - comparison operator(here we use its value from enum)
+     * @param fieldValue - value of the above-mentioned field
+     * @return - true if the object fits for conditions, else false
+     * @throws JSONException if JSON object is incorrect
+     */
     private static Boolean isCorrectJsonObject(JSONObject jsonObject, String field, Operator operator, Object fieldValue) throws JSONException {
         String[] keys = JSONObject.getNames(jsonObject);
         if (keys != null) {
@@ -146,6 +160,15 @@ public class JsonFilter {
         return false;
     }
 
+    /**
+     * This method do the same action as previous, but for String value of operator
+     * @param jsonObject - JSON object to check
+     * @param field - field in JSON for comparing value of it and filtering
+     * @param operator - comparison operator(here we use its String value)
+     * @param fieldValue - value of the above-mentioned field
+     * @return - true if the object fits for conditions, else false
+     * @throws JSONException if JSON object is incorrect
+     */
     private static Boolean isCorrectJsonObject(JSONObject jsonObject, String field, String operator, Object fieldValue) throws JSONException {
         String[] keys = JSONObject.getNames(jsonObject);
         if (keys != null) {
@@ -163,6 +186,14 @@ public class JsonFilter {
         return false;
     }
 
+    /**
+     * This method exactly compares actual value of the field
+     * with value that we entered to filter
+     * @param compareValue - value that we entered
+     * @param operator - comparison operator(here we use its String value)
+     * @param factValue - actual value of the field
+     * @return - true if comparing has been done successfully, else false
+     */
     private static boolean compareValues(Object compareValue, String operator, Object factValue) {
         return switch (operator) {
             case "=" -> compareValue.equals(factValue);
@@ -175,6 +206,13 @@ public class JsonFilter {
         };
     }
 
+    /**
+     * This method is same as previous? but fot enum value of operator
+     * @param compareValue - value that we entered
+     * @param operator - comparison operator(here we use its value from enum)
+     * @param factValue - actual value of the field
+     * @return - true if comparing has been done successfully, else false
+     */
     private static boolean compareValues(Object compareValue, Operator operator, Object factValue) {
         return switch (operator) {
             case EQUALS -> compareValue.equals(factValue);
@@ -184,5 +222,37 @@ public class JsonFilter {
             case GREATER_THAN_OR_EQUALS -> (Integer) compareValue >= (Integer) factValue;
             case LESS_THAN_OR_EQUALS -> (Integer) compareValue <= (Integer) factValue;
         };
+    }
+
+    /**
+     * This method is for converting filtered JSON objects to HashMap
+     * @param jsonObject - filtered JSON object
+     * @return - HashMap of filtered JSON objects
+     * @throws JSONException - if some of JSON objects are incorrect(or something else)
+     */
+    private static HashMap<String, Object> toMap(JSONObject jsonObject) throws JSONException {
+        HashMap<String, Object> map = new HashMap<>();
+        Iterator keysItr = jsonObject.keys();
+        while (keysItr.hasNext()) {
+            String key = (String) keysItr.next();
+            Object value = jsonObject.get(key);
+            if (value instanceof JSONObject) {
+                value = toMap((JSONObject) value);
+                map.put(key, value);
+            } else if (value instanceof JSONArray) {
+                ArrayList<Object> arrayValues = new ArrayList<>();
+                for (int i = 0; i < ((JSONArray) value).length(); i++) {
+                    if (((JSONArray) value).get(i) instanceof JSONObject) {
+                        arrayValues.add(toMap((JSONObject) ((JSONArray) value).get(i)));
+                    } else {
+                        arrayValues.add(((JSONArray) value).get(i));
+                    }
+                }
+                map.put(key, arrayValues);
+            } else {
+                map.put(key, value);
+            }
+        }
+        return map;
     }
 }
